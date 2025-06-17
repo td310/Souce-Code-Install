db0225dd4acdd8571af23cc10f1d5a1bfd3583ed
Tai <ndtai0912@gmail.com>
2025-06-17 22:00:33 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.main')
+@section('contents')
+    <section class="content">
+        <div class="container-fluid">
+            <style>
+                h2 {
+                    color: rgb(224, 224, 7);
+                    background: white;
+                    padding: 10px;
+                    text-align: center;
+                    top:40%;
+                    position: absolute;
+                    left: 50%;
+                }
+            </style>
+
+            <div class="text-center">
+                <h2>Nguyễn Đức Tài</h2>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/layout/extends/side-bar.blade.php
@@ -0,0 +1,18 @@
+<!-- Brand Logo -->
+<a href="../../index3.html" class="brand-link">
+    <img src="{{ asset('vendor/adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+        class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">AdminLTE 3</span>
+</a>
+
+<!-- Sidebar -->
+<div class="sidebar">
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+</div>
+<!-- /.sidebar -->
