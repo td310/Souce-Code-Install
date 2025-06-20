1ec0c8cc6ce206358d0cc0e98cbc2f58a18a2c84
Tai <ndtai0912@gmail.com>
2025-06-20 19:09:18 +0700

    update

--- app/Jobs/SendWelcomeEmailJob.php
+++ app/Jobs/SendWelcomeEmailJob.php
@@ -3,10 +3,8 @@
 namespace App\Jobs;
 
 use App\Models\User;
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\WelcomeEmail;
