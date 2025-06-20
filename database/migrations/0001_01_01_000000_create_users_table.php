6c94fb68935de280909ed709bb1325995dc2aa55
Tai <ndtai0912@gmail.com>
2025-06-20 15:54:41 +0700

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('address')->nullable();
-            $table->enum('status', [0, 1, 2, 3])->default(0);
+            $table->enum('status', ['0', '1', '2', '3'])->default('0');
             $table->enum('role', ['admin', 'user'])->default('user');
             $table->timestamps();
         });
